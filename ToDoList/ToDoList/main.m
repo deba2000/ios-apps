e2fd14b4cc58a1cfb793ba2a53153aafd7c5c9a6
Paul Rozdeba <paul.rozdeba@gmail.com>
2014-01-25 20:39:45 -0800

    initial commit

---
+++ ToDoList/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Paul Rozdeba on 1/25/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+	}
+}
